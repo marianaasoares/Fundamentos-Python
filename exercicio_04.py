d9ea954b6e8026e865489d0d31082be92a25ba18
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2019-09-19 09:06:46 -0300

    Exercicio 04
    Escreva um programa em Python que leia um vetor de 5 números inteiros e o apresente na ordem inversa.
    Imprima o vetor no final. Use listas.
    Exemplo: se a entrada for [4, 3, 5, 1, 2], o resultado deve ser [2, 1, 5, 3, 4].

---
+++ exercicio_04.py
@@ -0,0 +1,10 @@
+"""Escreva um programa em Python que leia um vetor de 5 números inteiros e o apresente na ordem inversa.
+Imprima o vetor no final. Use listas.
+Exemplo: se a entrada for [4, 3, 5, 1, 2], o resultado deve ser [2, 1, 5, 3, 4]."""
+
+lista_numeros = []
+
+while lista_numeros.items() == 5:
+    n = int(input("Digite os números para o programa ordenar inversamente: "))
+    lista_numeros.append(n)
+print(lista_numeros)
