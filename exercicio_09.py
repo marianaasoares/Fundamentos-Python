e7b625752caf75bc0cd667abb82e38695fbfcc07
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2019-09-26 00:29:42 -0300

    Tentativa de mover o círculo
    li o exercício errado e refiz

--- exercicio_09.py
+++ exercicio_09.py
@@ -4,56 +4,108 @@ ele movimente o círculo com o texto“ clique” nas direções corretas.Caso c
 
 
 import pygame
+from pygame.locals import *
+import pygame
+import random
 
 pygame.init()
 
-largura = 800
-altura = 600
+largura = 600
+altura = 400
 
+verde_escuro = (155, 225, 152)
+verde_claro = (173, 223, 179)
+branco = (255, 255, 255)
+preto = (0, 0, 0)
+vermelho = (255, 0, 0) 
 tela = pygame.display.set_mode((largura, altura))
 
-preto = (0, 0, 0)
-branco = (255, 255, 255)
+def botao(mensagem, x, y, raio, clica = None):
+    posicao_mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+        
+    if x+raio > posicao_mouse[0] > x and y+raio > posicao_mouse[1] > y:
+        pygame.draw.circle(tela, verde_claro, [x, y], raio)
+        if click[0] == 1 and clica != None:
+            if clica == "cliquei":
+                quadrado = Quadrado()
+                lista.append(quadrado)            
+    else:
+        pygame.draw.circle(tela, verde_escuro, [x, y], raio)  
+            
+    fonte = pygame.font.Font("Roboto-Thin.ttf", 20)
+    texto = fonte.render(mensagem, True, preto)
+    texto_retangulo = texto.get_rect()
+    texto_retangulo.center = ( (250+(100/2)), (75+(50/2)))
+    tela.blit(texto, texto_retangulo)
 
-x,y = 0
 
-moveX,moveY = 0
+class Quadrado():
+    def __init__(self):
+      self.largura = 50
+      self.altura = 50
+      self.x = random.randint(0, largura-self.largura)
+      self.y = random.randint(20, altura-self.altura)
+      self.area = pygame.Rect(self.x, self.y, self.largura, self.altura)
+      self.cor = vermelho
+    # funcao desenhar quadrado
+    def desenha(self, tela):
+      pygame.draw.rect(tela, self.cor, self.area)
 
-clock= pygame.time.Clock()
+lista = []
+
+for i in lista:
+    quadrado = Quadrado()
+    quadrado.desenha(tela)
+    lista.append(quadrado)
+
+y = 0
+x = 0
+moveX = 0
+moveY = 0 
+
 
 sair = False
 
 while not sair:
+    pygame.display.update()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sair = True
+            
+        tela.fill(branco)
+        botao("Clique", 300, 100, 50, "cliquei")
+        
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            quadrado = Quadrado()
+            lista.append(quadrado)
+            
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_d:
                 moveX = -5
-            if event.key == K_RIGHT:
+            if event.key == K_a:
                 moveX = 5
-            if event.key == K_UP:
+            if event.key == K_w:
                 moveY = -5
-            if event.key == K_DOWN:
+            if event.key == K_s:
                 moveY = 5
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_d:
                 moveX = 0
-            if event.key == K_RIGHT:
+            if event.key == K_a:
                 moveX = 0
-            if event.key == K_UP:
+            if event.key == K_w:
                 moveY = 0
-            if event.key == K_DOWN:
+            if event.key == K_s:
                 moveY = 0
-        tela.fill(branco)
+                
         x+=moveX
         y+=moveY
         
-        pygame.draw.circle(tela, preto, (x,y), 50)
+        for i in lista:
+            i.desenha(tela)
         
-        clock.tick(50)
-        
-        pygame.display.update()
-        
+                
+    
 pygame.quit()
-        
